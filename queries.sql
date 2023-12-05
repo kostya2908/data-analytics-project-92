3ec6c6ae8381a841b34743ef652838114b082746
kostya2908 <k.mokievets@gmail.com>
2023-12-05 19:57:30 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,74 @@
+--Запрос считает количество покупателей из таблицы customers по полю customer_id:
 select 
 count(customer_id) as customers_count
 from customers;
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+--Анализ отдела продаж ОТЧЕТ №1:
+--Запрос №1 (group by):
+with t as
+(
+select
+	concat(e.first_name, ' ', last_name) as name,
+	s.product_id,
+	sum(s.quantity) as operations,
+	p.price,
+	sum(s.quantity)*p.price as income
+from sales as s
+join employees as e on e.employee_id = s.sales_person_id
+join products as p on p.product_id = s.product_id
+group by 1, 2, 4
+)
+select 
+	t.name as name, 
+	sum(t.operations) as operations,
+	sum(t.income) as income
+from t
+group by 1
+order by 3 desc limit 10;
+-------------------------------------------------------------------------------------------
+--Анализ отдела продаж ОТЧЕТ №1:
+--Запрос №2 (partition by):
+with t as
+(
+select 
+	distinct concat(e.first_name, ' ', e.last_name) as full_name,
+	sum(s.quantity) over (partition by s.sales_person_id, s.product_id) as quantity_per_product,
+	(sum(s.quantity) over (partition by s.sales_person_id, s.product_id))*p.price as income_per_product
+from sales s
+join products p on p.product_id = s.product_id
+join employees e on e.employee_id = s.sales_person_id
+order by 1 asc
+)
+select
+	distinct full_name as name,
+	sum(quantity_per_product) over (partition by full_name) as operations,
+	sum(income_per_product) over (partition by full_name) as income
+from t
+order by 3 desc limit 10;
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+--Анализ отдела продаж ОТЧЕТ №2:
 
---запрос считает количество покупателей из таблицы customers по полю customer_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
